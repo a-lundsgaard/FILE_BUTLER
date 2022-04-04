579cb6fa75ba8406fb49c0677105430bbd369c73
a-lundsgaard <askelundsgaard@live.dk>
2022-04-04 17:30:28 +0200

    updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 
 import { useState } from 'react'
+import Link from 'next/link'
+import UploadImage from '../modules/uploadImage/uploadImage';
 
 interface Props {
   title: string
@@ -9,7 +11,6 @@ function Header({ title }: Props) {
   return <h1>{title ? title : 'Default title'}</h1>
 }
 
-import Link from 'next/link'
 //import "../styles/globals.css";
 
 
@@ -26,31 +27,7 @@ export default function HomePage() {
     <div>
       <Header title="Develop. Preview. Ship. 🚀" />
 
-      <div className="flex justify-center mt-8">
-        <div className="max-w-2xl rounded-lg shadow-xl bg-gray-50">
-          <div className="m-4">
-            <label className="inline-block mb-2 text-gray-500">File Upload</label>
-            <div className="flex items-center justify-center w-full">
-              <label
-                className="flex flex-col w-full h-32 border-4 border-blue-200 border-dashed hover:bg-gray-100 hover:border-gray-300">
-                <div className="flex flex-col items-center justify-center pt-7">
-                  <svg className="w-8 h-8 text-gray-400 group-hover:text-gray-600"
-                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
-                  </svg>
-                  <p className="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
-                    Attach a file</p>
-                </div>
-                <input type="file" className="opacity-0" />
-              </label>
-            </div>
-          </div>
-          <div className="flex justify-center p-2">
-            <button className={`w-full px-4 py-2 text-white bg-blue-500 rounded shadow-xl`}>Create</button>
-          </div>
-        </div>
-      </div>
+      <UploadImage/>
 
       <Link href="/posts/first-post">
         <a>this page!</a>
--- src/pages/posts/first-post.tsx
+++ src/pages/posts/first-post.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
+      <h1 className='bg-indigo-500'>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
