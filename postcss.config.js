b595d538c7d4f1510679aeaa16b7efd7d47f36f9
a-lundsgaard <askelundsgaard@live.dk>
2022-04-04 16:44:22 +0200

    Added files

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+// module.exports = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx}",
+//     "./components/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
