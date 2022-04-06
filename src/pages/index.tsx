c3700404c21559886180148628ca0693119f68e2
a-lundsgaard <askelundsgaard@live.dk>
2022-04-06 21:48:46 +0200

    updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,11 @@
-
-import { useState } from 'react'
-import Link from 'next/link'
 import UploadImage from '../modules/uploadImage/uploadImage';
 
-interface Props {
-  title: string
-}
-
-function Header({ title }: Props) {
-  return <h1>{title ? title : 'Default title'}</h1>
-}
-
-//import "../styles/globals.css";
-
-
 export default function HomePage() {
-
   return (
     <div>
       <div className="flex justify-center">
         <UploadImage />
       </div>
-
-      {/* <Link href="/posts/first-post">
-        <a>this page!</a>
-      </Link> */}
     </div>
   )
 }
