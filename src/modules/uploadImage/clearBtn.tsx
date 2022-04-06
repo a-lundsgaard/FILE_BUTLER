a9f6f2bee2a8cd411b11f1d066a0ae6adb15c426
a-lundsgaard <askelundsgaard@live.dk>
2022-04-06 21:45:46 +0200

    clear btn component

---
+++ src/modules/uploadImage/clearBtn.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ClearBtn() {
+    return (
+        <span
+            className="cursor-pointer material-icons p-1 pr-2 pl-2 rounded-lg bg-red-200 text-white font-bold border-black ">
+            X
+        </span>
+    )
+}
