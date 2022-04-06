7bb142ef047fabd5ea95eb6e9461682b5afbbe3a
a-lundsgaard <askelundsgaard@live.dk>
2022-04-06 21:45:21 +0200

    css target folders for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/common/**/*.{js,ts,jsx,tsx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
