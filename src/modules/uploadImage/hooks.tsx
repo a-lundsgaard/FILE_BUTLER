ffd789b00109ebd2eb1521926afcc1e0f23087ef
a-lundsgaard <askelundsgaard@live.dk>
2022-04-06 21:45:55 +0200

    upload hook

---
+++ src/modules/uploadImage/hooks.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import axios from "axios";
+
+export const useUploadForm = (url: string) => {
+  const [progress, setProgress] = useState(0);
+  
+  const uploadForm = async (formData: FormData) => {
+    return await axios.post(url, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      onUploadProgress: (progressEvent) => {
+        const progress = (progressEvent.loaded / progressEvent.total) * 50;
+        console.log(progress);
+        
+        setProgress(progress);
+      },
+      onDownloadProgress: (progressEvent) => {
+        const progress =  50 + (progressEvent.loaded / progressEvent.total) * 50;
+        console.log(progress);
+        
+        setProgress(progress);
+      },
+    });
+  };
+  return { uploadForm, progress };
+};
