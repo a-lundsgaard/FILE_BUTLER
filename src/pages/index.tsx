c541ba391cea1b17a9a7838b252d84164c88a033
a-lundsgaard <askelundsgaard@live.dk>
2022-04-13 08:44:33 +0200

    background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import UploadImage from '../modules/uploadImage/uploadImage';
 
 export default function HomePage() {
   return (
-    <div>
-      <div className="flex justify-center">
+    <div className='bg-gradient-to-t from-cyan-500 to-blue-500 h-screen '>
+      <div className="flex justify-center ">
         <UploadImage />
       </div>
     </div>
