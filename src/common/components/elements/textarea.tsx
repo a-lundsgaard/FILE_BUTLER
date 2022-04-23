78620cdb1d7d039934b47951792237a4e78d20b8
a-lundsgaard <askelundsgaard@live.dk>
2022-04-23 12:15:54 +0200

    Updated events on text value

--- src/common/components/elements/textarea.tsx
+++ src/common/components/elements/textarea.tsx
@@ -15,29 +15,10 @@ export default function Textarea({ placeholder, value, title }: Props) {
       </label>
       <textarea
         readOnly
-        className="
-          h-[20vh]
-          resize-none
-          form-control
-          block
-          w-full
-          px-3
-          py-1.5
-          text-base
-          font-normal
-          text-gray-700
-          bg-white bg-clip-padding
-          border border-solid border-gray-300
-          rounded
-          transition
-          ease-in-out
-          m-0
-          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" 
-
+        className={`ring-1  ${ value != '' && "h-[20vh] ring-blue-600 ring-2"} resize-none form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none`}
         id="exampleFormControlTextarea1"
         rows={3}
         placeholder={placeholder}
-        //onChange={ (e) => handleChange(e.target.value) }
         value={value}
       ></textarea>
     </div>
