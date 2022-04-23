7351d693dac93ca90516ec5d6693269438b1a344
a-lundsgaard <askelundsgaard@live.dk>
2022-04-23 15:15:05 +0200

    fixed navbar position and padding

--- src/common/components/navbar/navbarResponsive.tsx
+++ src/common/components/navbar/navbarResponsive.tsx
@@ -24,7 +24,7 @@ export default function NavbarResponsive() {
                 <div className='p-3' >
                     <Link href='/'>
                         <a className='inline-flex items-center p-2 mr-4 '>
-                            <Logo/> 
+                            <Logo />
                             <span className='text-xl text-white font-bold uppercase tracking-wide'>
                                 File butler
                             </span>
@@ -36,7 +36,7 @@ export default function NavbarResponsive() {
                     className=' inline-flex p-3 mr-3 hover:bg-indigo-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
                     onClick={handleClick}
                 >
-                    { !active ? <MenuIcon /> : <Plus className='rotate-45' /> }
+                    {!active ? <MenuIcon /> : <Plus className='rotate-45' />}
                 </button>
                 {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
                 <div
@@ -45,10 +45,10 @@ export default function NavbarResponsive() {
                 >
                     <div
                         onMouseLeave={() => setActive(false)}
-                        className={`z-50 shadow-md lg:shadow-none ${navBarColor} fixed lg:relative lg:inline-flex lg:flex-row lg:ml-auto  lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto`}>
+                        className={`z-50 shadow-md lg:shadow-none ${navBarColor} absolute lg:relative lg:inline-flex lg:flex-row lg:ml-auto  lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto`}>
                         {navLinks.map((link, index) => {
                             return <Link href={link.path} key={index} >
-                                <a className={` ${pathname === link.path && "bg-indigo-400"} lg:mr-2 lg:inline-flex lg:w-auto w-full px-3 py-3 lg:rounded text-white font-bold items-center justify-center hover:bg-indigo-700 hover:text-white`} key={index}>{link.name}</a>
+                                <a className={` ${pathname === link.path && "bg-indigo-400"}  lg:mr-2 lg:inline-flex lg:w-auto w-full px-3 pt-3 pb-4 lg:rounded text-white font-bold items-center justify-center hover:bg-indigo-700 hover:text-white`} key={index}>{link.name}</a>
                             </Link>
                         })
                         }
